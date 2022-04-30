82e38b42a7f81bc21855a5273c06735e9df6be84
Robert Kirykowicz <rakirykowicz@gmail.com>
2022-04-30 10:35:36 -0500

    created random background color generator with a click event

---
+++ DOM_Events_CODE/RandomColors/app.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+const randomColor = function () {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+};
+button.addEventListener("click", function () {
+  const newColor = randomColor();
+  document.querySelector("body").style.backgroundColor = newColor;
+  h1.innerText = `${newColor}`;
+});
